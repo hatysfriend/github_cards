002c16c5fa2c7583f88ac01e4a09044b6b2db1e1
hatysfriend <hatysfriend@gmail.com>
2021-06-28 22:02:20 +1000

    Setting up Card component + css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="header">{this.props.title}</div>
+        <Card/>
+      </>
+    )
+  }
 }
 
-export default App;
+export class Card extends Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src="http://placehold.it/75" alt=""></img>
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
